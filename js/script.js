31a983b0dda9444afb781538457396027ac3fecf
abdusharip <abdusharipa9@gmail.com>
2024-05-03 18:58:26 +0500

    added toTopBtn

--- js/script.js
+++ js/script.js
@@ -4,31 +4,31 @@ document.getElementById("nice");
 
 // men qo'shildim
 // abdusharip's codes
-window.addEventListener('scroll', function(){
-  const header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0)
-})
+// window.addEventListener('scroll', function(){
+//   const header = document.querySelector('header');
+//   header.classList.toggle('sticky', window.scrollY > 0)
+// })
 
-const scrollBtn =document.querySelector('.scrollToTop-btn')
-window.addEventListener('scroll', () => {
-    scrollBtn.classList.toggle('active', window.scrollY > 500)
-})
-scrollBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0
-    document.documentElement.scrollTop = 0
-})
+// const scrollBtn =document.querySelector('.scrollToTop-btn')
+// window.addEventListener('scroll', () => {
+//     scrollBtn.classList.toggle('active', window.scrollY > 500)
+// })
+// scrollBtn.addEventListener('click', () => {
+//     document.body.scrollTop = 0
+//     document.documentElement.scrollTop = 0
+// })
 
-window.addEventListener('scroll', () => {
-  let reveals = document.querySelectorAll('.reveal')
+// window.addEventListener('scroll', () => {
+//   let reveals = document.querySelectorAll('.reveal')
 
-  for(let i = 0; i < reveals.length; i++){
-      let windowHeight = window.innerHeight
-      let revealTop = reveals[i].getBoundingClientRect().top
-      let revalPoint = 50
+//   for(let i = 0; i < reveals.length; i++){
+//       let windowHeight = window.innerHeight
+//       let revealTop = reveals[i].getBoundingClientRect().top
+//       let revalPoint = 50
 
-      if(revealTop < windowHeight - revalPoint){
-          reveals[i].classList.add('active')
-      }
-  }
-})
+//       if(revealTop < windowHeight - revalPoint){
+//           reveals[i].classList.add('active')
+//       }
+//   }
+// })
 // abdusharip's codes
